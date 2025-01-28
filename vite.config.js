39fa23ca92d947004374acbaa2f11b6ac554d49b
ahsan-mahfuz <ahsanmahfuz0@gmail.com>
2025-01-28 15:46:11 +0600

    abc

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host:'192.168.10.25',
-    host: '0.0.0.0',
-    port: '7585',
+    host: '10.0.60.38',
+    // port: '7585',
   },
 })
